f70b11d91be302d3a05d20277ee2d733a192bcb6
Shahmeer Asim <shahmeerasim1999@gmail.com>
2024-05-18 03:13:03 +0500

    removed the dashboard folder, and added salesforce integration to the experience page

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -1,19 +1,74 @@
 "use client";
 
 import "./style.css";
-
 import jobsData from "../local database/jobData";
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Experience() {
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     Aos.init({
       duration: 500,
       once: true,
     });
-  });
+
+    const fetchData = async () => {
+      try {
+        const accessToken = await getAccessToken();
+        await getCurrentContactsData(accessToken);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      Aos.refresh(); // Refresh AOS when component unmounts
+    };
+  }, []);
+
+  const getAccessToken = async () => {
+    const data = `grant_type=password&username=${process.env.NEXT_PUBLIC_USERNAME}&password=${process.env.NEXT_PUBLIC_PASSWORD}&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_CLIENT_SECRET}`;
+    try {
+      setIsLoading(true);
+      const result = await fetch(process.env.NEXT_PUBLIC_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: data,
+      });
+      const responseBody = await result.json();
+      console.log("access token::: ", responseBody.access_token);
+      console.log("token type::: ", responseBody.token_type);
+      setIsLoading(false);
+      return responseBody.access_token;
+    } catch (error) {
+      console.log("There is an error::: ", error);
+      setIsLoading(false);
+    }
+  };
+
+  const getCurrentContactsData = async (accessToken) => {
+    try {
+      console.log("accessToken is::: ", accessToken);
+      const data = await fetch(process.env.NEXT_PUBLIC_SALESFORCE_ENDPOINT, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      });
+      const response = await data.json();
+      console.log("data from salesforce for the current user is::: ", response);
+    } catch (error) {
+      console.log("There was an error calling salesforce api::: ", error);
+    }
+  };
+
   return (
     <main>
       <section className="min-h-[25dvh] md:min-h-[45dvh] flex items-center">
@@ -27,40 +82,38 @@ export default function Experience() {
         </div>
         <div className="flex-1">
           {/* first job */}
-          {jobsData.map((jobData, index) => {
-            return (
+          {jobsData.map((jobData, index) => (
+            <div
+              key={index}
+              className="ml-2 md:ml-0 border-l-2 border-l-black job"
+            >
               <div
-                key={index}
-                className="ml-2 md:ml-0 border-l-2 border-l-black job"
+                data-aos="fade-up"
+                className="flex items-center justify-between max-w-[80%] font-bold pl-6"
               >
-                <div
-                  data-aos="fade-up"
-                  className="flex items-center justify-between max-w-[80%] font-bold pl-6"
-                >
-                  <div>
-                    <h1 className="uppercase text-sm">{jobData.Role}</h1>
-                    <p className="text-gray-400 uppercase text-xs">
-                      {jobData["Company Name"]}
-                    </p>
-                  </div>
-                  <div>
-                    <p className="text-gray-400 text-xs md:text-sm">
-                      {jobData["Start Month"]} {jobData["Start Year"]} -{" "}
-                      {jobData["End Month"]} {jobData["End Year"]}
-                    </p>
-                  </div>
+                <div>
+                  <h1 className="uppercase text-sm">{jobData.Role}</h1>
+                  <p className="text-gray-400 uppercase text-xs">
+                    {jobData["Company Name"]}
+                  </p>
                 </div>
-                <div
-                  data-aos="zoom-in"
-                  data-aos-duration="2000"
-                  className="pl-6 max-w-[70%] mt-6 text-sm"
-                >
-                  <p>{jobData["Job Description"]}</p>
+                <div>
+                  <p className="text-gray-400 text-xs md:text-sm">
+                    {jobData["Start Month"]} {jobData["Start Year"]} -{" "}
+                    {jobData["End Month"]} {jobData["End Year"]}
+                  </p>
                 </div>
-                <div className="py-7"></div>
               </div>
-            );
-          })}
+              <div
+                data-aos="zoom-in"
+                data-aos-duration="2000"
+                className="pl-6 max-w-[70%] mt-6 text-sm"
+              >
+                <p>{jobData["Job Description"]}</p>
+              </div>
+              <div className="py-7"></div>
+            </div>
+          ))}
         </div>
       </section>
 
@@ -70,42 +123,40 @@ export default function Experience() {
         </div>
         <div className="flex-1">
           {/* first job */}
-          {jobsData.map((jobData, index) => {
-            return (
+          {jobsData.map((jobData, index) => (
+            <div
+              key={index}
+              className="border-r-2 border-r-black certification"
+            >
               <div
-                key={index}
-                className="border-r-2 border-r-black certification"
+                data-aos="fade-up"
+                className="flex items-center justify-between max-w-[80%] font-bold pl-6"
               >
-                <div
-                  data-aos="fade-up"
-                  className="flex items-center justify-between max-w-[80%] font-bold pl-6"
-                >
-                  <div>
-                    <h1 className="uppercase">{jobData.Role}</h1>
-                    <p className="text-gray-400 uppercase text-xs">
-                      {jobData["Company Name"]}
-                    </p>
-                  </div>
-                  <div>
-                    <p className="text-gray-400 text-sm">
-                      {jobData["Start Month"]} {jobData["Start Year"]} -{" "}
-                      {jobData["End Month"]} {jobData["End Year"]}
-                    </p>
-                  </div>
+                <div>
+                  <h1 className="uppercase">{jobData.Role}</h1>
+                  <p className="text-gray-400 uppercase text-xs">
+                    {jobData["Company Name"]}
+                  </p>
                 </div>
-                <div
-                  data-aos="zoom-in"
-                  data-aos-duration="2000"
-                  className="pl-6 max-w-[70%] mt-6 text-sm"
-                >
-                  <p>{jobData["Job Description"]}</p>
+                <div>
+                  <p className="text-gray-400 text-sm">
+                    {jobData["Start Month"]} {jobData["Start Year"]} -{" "}
+                    {jobData["End Month"]} {jobData["End Year"]}
+                  </p>
                 </div>
-                <div className="py-7"></div>
               </div>
-            );
-          })}
+              <div
+                data-aos="zoom-in"
+                data-aos-duration="2000"
+                className="pl-6 max-w-[70%] mt-6 text-sm"
+              >
+                <p>{jobData["Job Description"]}</p>
+              </div>
+              <div className="py-7"></div>
+            </div>
+          ))}
         </div>
-        <div className=" hidden flex-1 md:flex items-center justify-center h-[100dvh] sticky top-0">
+        <div className="hidden flex-1 md:flex items-center justify-center h-[100dvh] sticky top-0">
           <h1 className="text-2xl font-black">Certifications</h1>
         </div>
       </section>
